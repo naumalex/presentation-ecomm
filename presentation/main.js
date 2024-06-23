efe20a42383923094417a0a8ff82804d86cf9195
Sergei <106452081+Fidyi@users.noreply.github.com>
2024-06-23 22:01:55 +0300

    add git section

--- presentation/main.js
+++ presentation/main.js
@@ -1,10 +1,6 @@
 import Reveal from 'reveal.js';
 import 'reveal.js/dist/reveal.css';
 import hljs from 'highlight.js';
-// see available themes in the
-// node_modules/reveal.js/dist/theme
-//  beige, black, blood, league, moon, night, serif, simple, ...
-// import ComparisonSlider from "comparison-slider";
 import 'reveal.js/dist/theme/black.css';
 
 const deck = new Reveal();
